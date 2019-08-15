19ba91d0084ba381c17fe0fb96b2cbfd795b79ec
nick <nleslie@landmarkschool.org>
2019-08-15 15:47:37 -0400

    enemy AI

--- polarity game/Assets/EnemyControler.cs
+++ polarity game/Assets/EnemyControler.cs
@@ -17,18 +17,36 @@ public float waitTime;
 //UI
 public Image inZone;
 public Image alerted;
+//---------------------
+//AI 
+public NavMeshAgent agent;
+public Transform[] wayPoints;
+int m_curentwaypointIndex;
+public float AIwaitTime;
+public float AImaxWaitTime;
+//---------------------
+// enemy health and dying
+public int health;
     // Start is called before the first frame update
     void Start()
     {
        player=GameObject.FindGameObjectWithTag("Player");
        sight=gameObject.GetComponent<SphereCollider>();
        inZone.enabled=false;
+       agent.SetDestination(wayPoints[0].position);
     }
 
     // Update is called once per frame
     void Update()
     {
         DetectionManinger();
+        if(playerSeen==true) {
+            agent.isStopped=true;
+            gameObject.transform.LookAt(player.transform.position,Vector3.up);
+        } else {
+            agent.isStopped=false;
+        }
+           patroling();
     }
 
 private void DetectionManinger() {
@@ -66,5 +84,18 @@ private void DetectionManinger() {
         waitTime=0;
     }
 }
-
+public void patroling() {
+if(agent.remainingDistance <= agent.stoppingDistance) {
+if (AIwaitTime>=AImaxWaitTime) {
+    m_curentwaypointIndex= (m_curentwaypointIndex + 1) % wayPoints.Length;
+    agent.SetDestination(wayPoints[m_curentwaypointIndex].position);
+    AIwaitTime=0;
+} else {
+    AIwaitTime+=Time.deltaTime;
+}
+}
+}
+public void Damage(int damage) {
+    
+}
 }
