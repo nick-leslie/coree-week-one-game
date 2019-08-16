fdcc63348535271954265634136f811e28e086a9
nick <nleslie@landmarkschool.org>
2019-08-16 14:48:41 -0400

    added stage (wip)

---
+++ polarity game/Assets/YouWin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class YouWin : MonoBehaviour
+{
+    public Text Youwin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+private void OnCollisionEnter(Collision other) {
+        if(other.collider.CompareTag("Player")) {
+            Youwin.enabled=true;
+        }
+    }
+}
